d6c2e4a4d0f93ecec62da6ae091f425942444bef
invoke <75154889+PranavPurwar@users.noreply.github.com>
2026-03-03 11:18:49 +0530

    Bump version from 1.0.0 to 1.1.0

--- filepicker/build.gradle.kts
+++ filepicker/build.gradle.kts
@@ -49,7 +49,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.PranavPurwar"
                 artifactId = "filepicker"
-                version = "1.0.0"
+                version = "1.1.0"
             }
         }
     }
